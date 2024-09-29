66bf51e21e54555415d3d596524f9b2949629ed9
Zain <ahmedzain958@gmail.com>
2024-09-29 23:05:44 +0300

    gym till Retrofit Section: Understanding Basic Definitions & Creating Firebase Realtime Database

--- app/src/main/java/com/zainco/androidcookiescompose/GymsViewModel.kt
+++ app/src/main/java/com/zainco/androidcookiescompose/GymsViewModel.kt
@@ -2,13 +2,13 @@ package com.zainco.androidcookiescompose
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class GymsViewModel : ViewModel() {
+class GymsViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
     var state by
-        mutableStateOf(getGyms())
+        mutableStateOf(restoreGymsAndTheSelected())
 
     private fun getGyms() = listOfGyms
 
@@ -16,6 +16,27 @@ class GymsViewModel : ViewModel() {
         val gyms = state.toMutableList()
         val itemIndex = gyms.indexOfFirst { it.id == gymId }
         gyms[itemIndex] = gyms[itemIndex].copy(isFavorite = !gyms[itemIndex].isFavorite)
+        storeSelectedGym(gyms[itemIndex])
         state = gyms
+    }
+    fun restoreGymsAndTheSelected(): List<Gym>{
+        val gyms = getGyms()
+        stateHandle.get<List<Int>>(FAV_IDS)?.let { savedIds ->
+            savedIds.forEach { gymId ->
+                gyms.find { it.id == gymId }?.isFavorite = true
+            }
+        }
+        return gyms
+    }
+    private fun storeSelectedGym(gym: Gym){
+        val savedHandleList = stateHandle.get<List<Int>>(FAV_IDS).orEmpty().toMutableList()
+        if(gym.isFavorite)
+            savedHandleList.add(gym.id)
+        else
+            savedHandleList.remove(gym.id)
+        stateHandle[FAV_IDS] = savedHandleList
+    }
+    companion object{
+        const val FAV_IDS= "favouriteGymsIds"
     }
 }
