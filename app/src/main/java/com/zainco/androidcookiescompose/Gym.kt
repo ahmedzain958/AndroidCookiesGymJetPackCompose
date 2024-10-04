3c04e4881cb168e7f250781485ef99c08e4708e6
Zain <ahmedzain958@gmail.com>
2024-10-05 00:49:17 +0300

    gym till Jetpack Room: Understand Caching in Android and How Room Works

--- app/src/main/java/com/zainco/androidcookiescompose/Gym.kt
+++ app/src/main/java/com/zainco/androidcookiescompose/Gym.kt
@@ -1,10 +1,20 @@
 package com.zainco.androidcookiescompose
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 
-data class Gym(val id: Int,
-               @SerializedName("gym_name") val name: String,
-               @SerializedName("gym_location") val place: String,
-               @SerializedName("is_open")
-               val isOpen: Boolean,
-               var isFavorite: Boolean = false)
+@Entity(tableName = "gyms")
+data class Gym(
+    @ColumnInfo(name = "gym_id")
+    val id: Int,
+    @ColumnInfo(name = "gym_name")
+    @SerializedName("gym_name")
+    val name: String,
+    @ColumnInfo(name = "gym_location")
+    @SerializedName("gym_location")
+    val place: String,
+    @SerializedName("is_open")
+    val isOpen: Boolean,
+    var isFavorite: Boolean = false,
+)
---
+++ app/src/main/java/com/zainco/androidcookiescompose/GymsDAO.kt
@@ -0,0 +1,15 @@
+package com.zainco.androidcookiescompose
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface GymsDAO {
+    @Query("SELECT * FROM gyms")
+    suspend fun getAll(): List<Gym>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAll(gyms: List<Gym>)
+}
